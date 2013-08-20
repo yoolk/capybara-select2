30cc1bb67813dc4263236ae7a13e4cc158fce506
Vuthy San <saravuthy@gmail.com>
2013-08-21 06:48:46 +0700

    Put back original select2 method and use select2_ajax for testing select2 with ajax

--- gem/lib/capybara-select2.rb
+++ gem/lib/capybara-select2.rb
@@ -3,7 +3,7 @@ require 'rspec/core'
 
 module Capybara
   module Select2
-    def select2(value, options = {})
+    def select2_ajax(value, options = {})
       raise "Must pass a hash containing 'from' or 'xpath'" unless options.is_a?(Hash) and [:from, :xpath].any? { |k| options.has_key? k }
 
       if options.has_key? :xpath
@@ -22,6 +22,32 @@ module Capybara
       end
 
       [value].flatten.each do |value|
+        find(:xpath, "//body").find("#{drop_container} li", text: value).click
+      end
+    end
+
+    def select2(value, options = {})
+      raise "Must pass a hash containing 'from' or 'xpath'" unless options.is_a?(Hash) and [:from, :xpath].any? { |k| options.has_key? k }
+
+      if options.has_key? :xpath
+        select2_container = first(:xpath, options[:xpath])
+      else
+        select_name = options[:from]
+        select2_container = first("label", text: select_name).find(:xpath, '..').find(".select2-container")
+      end
+
+      select2_container.find(".select2-choice").click
+
+      if options.has_key? :search
+        find(:xpath, "//body").find("input.select2-input").set(value)
+        page.execute_script(%|$("input.select2-input:visible").keyup();|)
+        drop_container = ".select2-results"
+      else
+        drop_container = ".select2-drop"
+      end
+
+      [value].flatten.each do |value|
+        select2_container.find(:xpath, "a[contains(concat(' ',normalize-space(@class),' '),' select2-choice ')] | ul[contains(concat(' ',normalize-space(@class),' '),' select2-choices ')]").click
         find(:xpath, "//body").find("#{drop_container} li", text: value).click
       end
     end

10331babc14d9da5e576f39518719838d67482e3
Vuthy San <saravuthy@gmail.com>
2013-08-21 07:42:12 +0700

    add select2 multiple choices support

--- gem/lib/capybara-select2.rb
+++ gem/lib/capybara-select2.rb
@@ -51,6 +51,18 @@ module Capybara
         find(:xpath, "//body").find("#{drop_container} li", text: value).click
       end
     end
+
+    def select2_multiple(value, options = {})
+      raise "Must pass a hash containing 'from'" if not options.is_a?(Hash) or not options.has_key?(:from)
+      select_name = options[:from]
+
+      select2_container=first("label", text: select_name).find(:xpath, '..').find(".select2-container")
+
+      [value].flatten.each do |value|
+        select2_container.find(:xpath, "a[contains(concat(' ',normalize-space(@class),' '),' select2-choice ')] | ul[contains(concat(' ',normalize-space(@class),' '),' select2-choices ')]").click
+        find(:xpath, "//body").find(".select2-drop li", text: value).click
+      end
+    end
   end
 end
 

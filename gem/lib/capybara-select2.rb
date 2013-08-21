08d55a075e325e90e1d9cae9c87f2e0d38d2031f
Vuthy San <saravuthy@gmail.com>
2013-08-21 10:34:38 +0700

    Add parent class to specific finding when there are more than two .select2-input

--- gem/lib/capybara-select2.rb
+++ gem/lib/capybara-select2.rb
@@ -3,29 +3,6 @@ require 'rspec/core'
 
 module Capybara
   module Select2
-    def select2_ajax(value, options = {})
-      raise "Must pass a hash containing 'from' or 'xpath'" unless options.is_a?(Hash) and [:from, :xpath].any? { |k| options.has_key? k }
-
-      if options.has_key? :xpath
-        select2_container = first(:xpath, options[:xpath])
-      else
-        select_name = options[:from]
-        select2_container = first("label", text: select_name).find(:xpath, '..').find(".select2-container")
-      end
-
-      select2_container.find(".select2-choice").click
-
-      drop_container = '.select2-drop'
-      if options.has_key? :search
-        find(:xpath, "//body").find("input.select2-input").set(value)
-        page.execute_script(%|$("input.select2-input:visible").keyup();|)
-      end
-
-      [value].flatten.each do |value|
-        find(:xpath, "//body").find("#{drop_container} li", text: value).click
-      end
-    end
-
     def select2(value, options = {})
       raise "Must pass a hash containing 'from' or 'xpath'" unless options.is_a?(Hash) and [:from, :xpath].any? { |k| options.has_key? k }
 
@@ -48,6 +25,34 @@ module Capybara
 
       [value].flatten.each do |value|
         select2_container.find(:xpath, "a[contains(concat(' ',normalize-space(@class),' '),' select2-choice ')] | ul[contains(concat(' ',normalize-space(@class),' '),' select2-choices ')]").click
+        find(:xpath, "//body").find("#{drop_container} li", text: value).click
+      end
+    end
+    
+    def select2_ajax(value, options = {})
+      raise "Must pass a hash containing 'from' or 'xpath'" unless options.is_a?(Hash) and [:from, :xpath].any? { |k| options.has_key? k }
+
+      if options.has_key? :xpath
+        select2_container = first(:xpath, options[:xpath])
+      else
+        select_name = options[:from]
+        select2_container = first("label", text: select_name).find(:xpath, '..').find(".select2-container")
+      end
+
+      select2_container.find(".select2-choice").click
+
+      drop_container = '.select2-drop'
+      if options.has_key? :search
+        if options.has_key? :parent
+          find(:xpath, "//body").find("#{options[:parent]} input.select2-input").set(value)
+          page.execute_script(%|$("#{options[:parent]} input.select2-input:visible").keyup();|)
+        else
+          find(:xpath, "//body").find("input.select2-input").set(value)
+          page.execute_script(%|$("input.select2-input:visible").keyup();|)
+        end
+      end
+
+      [value].flatten.each do |value|
         find(:xpath, "//body").find("#{drop_container} li", text: value).click
       end
     end
